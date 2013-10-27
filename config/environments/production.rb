a7ac9040b39efb5e722de2c2d9065a06f2a0aba0
mickeymick25 <mickeymick25@gmail.com>
2013-10-27 21:51:45 +0100

    production config assets for precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ PlessBootstrap::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
