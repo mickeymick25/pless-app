c688d4c743abdbb87a64bb5037394024fb4a6043
mickeymick25 <mickeymick25@gmail.com>
2013-10-27 21:32:47 +0100

    change production.rb for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,21 @@ PlessBootstrap::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'smartpless.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"  
+
+    config.action_mailer.smtp_settings = {
+      :address   => "smtp.mandrillapp.com",
+      :port      => 587,
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_API_KEY"]
+    }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
@@ -71,14 +86,6 @@ PlessBootstrap::Application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify  
-
-    config.action_mailer.smtp_settings = {
-      :address   => "smtp.mandrillapp.com",
-      :port      => 587,
-      :user_name => ENV["MANDRILL_USERNAME"],
-      :password  => ENV["MANDRILL_API_KEY"]
-    }
-
 
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
