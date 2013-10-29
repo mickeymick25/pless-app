2318baa0075e796300a12607745ecac8ab864881
mickeymick25 <mickeymick25@gmail.com>
2013-10-29 23:01:37 +0100

    creation of dashboard blank page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 PlessBootstrap::Application.routes.draw do
 
+#	root :to => "home#index"
+#	devise_for :users, :controllers => {:registrations => "registrations"}
+#	resources :users
+
 	root :to => "home#index"
-	devise_for :users, :controllers => {:registrations => "registrations"}
+	devise_for :users, :path => '', :path_names => {:registrations => "registrations", :sign_in => 'login', :sign_out => 'logout'}
 	resources :users
 
+	unauthenticated do
+		root to: "home#index", as: :unauthenticated_root
+	end
+
+	authenticated :user do
+		root to: "dashboard#index", as: :authenticated_root
+	end
 end
